1a07c4c858f6ce1d4fd1a34e3bc3e338617189ce
PoojithaAlam <poojithaalam@gmail.com>
2023-11-20 10:46:55 -0800

    Created Tv show Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TvshowComponent } from './tvshow/tvshow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TvshowComponent
   ],
   imports: [
     BrowserModule
